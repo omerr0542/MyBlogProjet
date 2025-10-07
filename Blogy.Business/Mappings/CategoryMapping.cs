ad94620501bc8ea9b0efa5a41cb4d67371c4edf5
omerr0542 <o.faruk0542@gmail.com>
2025-10-07 21:27:57 +0300

    initial

--- Blogy.Business/Mappings/CategoryMapping.cs
+++ Blogy.Business/Mappings/CategoryMapping.cs
@@ -10,7 +10,7 @@ public class CategoryMapping : Profile
     {
         CreateMap<Category, ResultCategoryDto>().ReverseMap();
         CreateMap<Category, UpdateCategoryDto>().ReverseMap();
-        CreateMap<Category, CreateCategoryDto>().ReverseMap(); // CreateCategoryDto'dan Category'e ve tersine dönüşüm için ReverseMap() ekledik.
+        CreateMap<Category, CreateCategoryDto>().ReverseMap(); // CreateCategoryDto'dan Category'e ve tersine dönüşüm için ReverseMap() ekledik. 
 
         // Eğer property isimleri farklı olsaydı, ForMember kullanarak manuel eşleme yapabilirdik.
     }
