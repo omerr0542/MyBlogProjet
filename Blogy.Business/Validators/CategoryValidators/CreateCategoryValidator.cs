2388f609fc23ee2130fe5111fd441b24912c1444
omerr0542 <o.faruk0542@gmail.com>
2025-10-07 21:52:54 +0300

    Category Dtos Validators Added

---
+++ Blogy.Business/Validators/CategoryValidators/CreateCategoryValidator.cs
@@ -0,0 +1,15 @@
+﻿using Blogy.Business.DTOs.CategoryDtos;
+using FluentValidation;
+
+namespace Blogy.Business.Validators.CategoryValidators;
+
+public class CreateCategoryValidator : AbstractValidator<CreateCategoryDto>
+{
+    public CreateCategoryValidator()
+    {
+        RuleFor(c => c.Name) // Hangi Property'ye kural uygulayacağımızı belirtir.
+            .NotEmpty().WithMessage("Kategori Adı Boş Bırakılamaz!")
+            .MinimumLength(3).WithMessage("Kategori Adı Minimum 3 Karakter Olmalı")
+            .MaximumLength(50).WithMessage("Kategori Adı Maximum 50 Karakter Olabilir");
+    }
+}
---
+++ Blogy.Business/Validators/CategoryValidators/UpdateCategoryValidator.cs
@@ -0,0 +1,15 @@
+﻿using Blogy.Business.DTOs.CategoryDtos;
+using FluentValidation;
+
+namespace Blogy.Business.Validators.CategoryValidators;
+
+public class UpdateCategoryValidator : AbstractValidator<UpdateCategoryDto>
+{
+    public UpdateCategoryValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty().WithMessage("Kategori Adı Boş Bırakılamaz!")
+            .MinimumLength(3).WithMessage("Kategori Adı Minimum 3 Karakter Olmalı")
+            .MaximumLength(50).WithMessage("Kategori Adı Maximum 50 Karakter Olabilir");
+    }
+}
